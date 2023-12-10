d202324fbb0c6f00803223411aaa92bde7d042f9
Iwema Onoriodeclear <onosfavour16@gmail.com>
2023-12-10 23:45:38 +0100

    area of a function

---
+++ area_of_triangle.js
@@ -0,0 +1,10 @@
+function calculateTriangleArea(base, height) {
+        return base * height / 2;
+  }
+ 
+//   Example usage
+  let base= 12;
+  let height = 12;
+  
+  let area = calculateTriangleArea(base,height);
+  console.log('The Area of the Triangle is:', area);
